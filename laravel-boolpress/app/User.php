7e52d5fa42d2d4260c6ce20ddc7cd5990159da46
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-11-09 17:09:59 +0100

    Creati model, seeder, migrations

--- laravel-boolpress/app/User.php
+++ laravel-boolpress/app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Article;
 
 class User extends Authenticatable
 {
@@ -36,4 +37,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function articles()
+    {
+      return $this->hasMany("App\Article");
+    }
 }
---
+++ laravel-boolpress/database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Article;
+use App\User;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+      for($i = 0; $i < 10; $i++){
+        $user = User::inRandomOrder()->first();
+
+        $newArticle = new Article;
+
+        $newArticle->user_id = $user->id;
+        $newArticle->title = $faker->sentence(3, true);
+        $newArticle->content = $faker->paragraph(3, true);
+        $newArticle->slug = Str::of($newArticle->title)->slug("-");
+
+        $newArticle->save();
+      }
+    }
+}
